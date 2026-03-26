c6cdfbcec3ef1fd2a1cd16ce7be3b16aff59dcfd
Ben Lacey <85163278+benlacey57@users.noreply.github.com>
2026-03-26 02:48:01 +0000

    Add HTML structure to header.php

--- projects/ideal-glass/header.php
+++ projects/ideal-glass/header.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE html>
+<html lang="en">
+<?php 
+// Load the head section
+if (file_exists('head.php')) {
+    include_once 'head.php';
+} 
+?>
+  
+<body>
+    <header>
+        <nav>
+          Header
+        </nav>
+    </header>
